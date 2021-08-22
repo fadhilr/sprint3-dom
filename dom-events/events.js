0c743d913f8885da08088e612430e7228eeaa949
fadhilr <fadilrizqullah97@gmail.com>
2021-08-22 10:59:35 +0700

    Commit belajar sprint 3

--- dom-events/events.js
+++ dom-events/events.js
@@ -1,3 +1,30 @@
 //events handler
+const p2 = document.querySelector('.p2')
 
+function ubahWarna2() {
+    p2.style.backgroundColor = 'orange'
+}
+
+p2.onclick = ubahWarna2
 //addEventListener()
+
+
+const p3 = document.querySelector('.p3')
+
+function ubahWarna() {
+    p3.style.backgroundColor = 'orange'
+}
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'Blue'
+})
+
+btn.addEventListener('mouseleave', function () {
+    p2.style.backgroundColor = 'Blue'
+})
+
+btn.addEventListener('mouseenter', function () {
+    p2.style.backgroundColor = 'Green'
+})
